586b3ec55359d732cf09c8a2938a5bb702d1f76a
James Ee <jee@c2r6s2.42singapore.sg>
2023-09-07 17:39:42 +0800

    copied sutd termail settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/james/miniconda3/bin/conda
-    eval /Users/james/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-
-tmux setenv TMUXPWD $PWD
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-export USER=jee
 export MAIL=jee@student.42singapore.sg
+
